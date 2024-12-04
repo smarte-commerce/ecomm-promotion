9e1989b09f378ff36003f47a2a73e29949c45c57
Win Nguyen <winnguyen1905.work@gmail.com>
2024-12-04 17:38:00 +0700

    init service

---
+++ src/test/java/com/winnguyen1905/promotion/PromotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winnguyen1905.promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
